1cffe3f8dd7926a8a50ce939b7660b49f6fc7c76
gudrum <gudrum.dev@gmail.com>
2024-02-14 00:49:24 -0500

    update

--- app/Providers/TestingServiceProvider.php
+++ app/Providers/TestingServiceProvider.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Testing\TestResponse;
 use Inertia\Testing\AssertableInertia;
-
 class TestingServiceProvider extends ServiceProvider
 {
     /**
@@ -17,7 +14,6 @@ class TestingServiceProvider extends ServiceProvider
     {
         //
     }
-
     /**
      * Bootstrap services.
      */
@@ -26,27 +22,25 @@ class TestingServiceProvider extends ServiceProvider
         if (! $this->app->runningUnitTests()) {
             return;
         }
-
         AssertableInertia::macro('hasResource', function (string $key, JsonResource $resource) {
             $this->has($key);
             expect($this->prop($key))->toEqual($resource->response()->getData(true));
-
             return $this;
         });
-
         AssertableInertia::macro('hasPaginatedResource', function (string $key, ResourceCollection $resource) {
             $this->hasResource("{$key}.data", $resource);
             expect($this->prop($key))->toHaveKeys(['data', 'links', 'meta']);
-
             return $this;
         });
-
         TestResponse::macro('assertHasResource', function (string $key, JsonResource $resource) {
             return $this->assertInertia(fn (AssertableInertia $inertia) => $inertia->hasResource($key, $resource));
         });
-
         TestResponse::macro('assertHasPaginatedResource', function (string $key, ResourceCollection $resource) {
             return $this->assertInertia(fn (AssertableInertia $inertia) => $inertia->hasPaginatedResource($key, $resource));
+        });
+
+        TestResponse::macro('assertComponent', function (string $component) {
+            return $this->assertInertia(fn (AssertableInertia $inertia) => $inertia->component($component, true));
         });
     }
 }
